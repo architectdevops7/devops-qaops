851a5d31d501ef6643c997fcb6df34bbce4451ea
ChetanRamesh <chetanramesh@ChetanRameshs-MacBook-Pro.local>
2023-12-10 14:50:22 +0530

    adding file

--- src/test/java/com/example/FailingTest.java
+++ src/test/java/com/example/FailingTest.java
@@ -7,14 +7,8 @@ public class FailingTest {
 
     @Test
     public void testNullPointerException() {
-    String text = null;
-    // Check for null before calling length()
-    if (text != null) {
+        String text = null;
+        // Introduce a NullPointerException
         Assert.assertEquals(text.length(), 5);
-    } else {
-        // Handle the case where text is null
-        // For example, log a message or fail the test with an appropriate message
-        Assert.fail("Text is null");
-    }
     }
 }
